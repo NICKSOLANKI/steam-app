1faec45cd1d9722b432f1f4a868ab0babd2ebfe9
Nick Solanki <nick@steam-app.local>
2026-09-11 13:38:37 +0530

    Configure strict SSL CA path for TiDB Cloud connection

--- config/database.php
+++ config/database.php
@@ -58,11 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => array_merge([
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ], defined('PDO::MYSQL_ATTR_SSL_CA') ? [
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ] : []),
+            ],
         ],
 
         'pgsql' => [
